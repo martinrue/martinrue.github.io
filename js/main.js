8dcbae7911600e6643657a29d663251f0de96f73
Martin Rue <hello@martinrue.com>
2018-02-17 14:04:36 +0000

    add fix to blog button being pressed twice

--- js/main.js
+++ js/main.js
@@ -14,6 +14,7 @@ $(document).ready(function () {
     } else {
       $('.panel-cover').css('max-width', currentWidth)
       $('.panel-cover').animate({'max-width': '530px', 'width': '40%'}, 400, swing = 'swing', function () {})
+      $('.panel-cover').addClass('panel-cover--collapsed')
     }
   })
 
