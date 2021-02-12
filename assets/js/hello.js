d3f742b2807c72da915c7c70965a40b722338962
Martin Rue <hello@martinrue.com>
2021-02-12 21:30:07 +0000

    Update homepage and fix greeting text code to repeat animations

--- assets/js/hello.js
+++ assets/js/hello.js
@@ -1,32 +1,32 @@
 const hello = (() => {
   const start = () => {
-    const $english = lib.$(".typewriter .english");
-    const $hebrew = lib.$(".typewriter .hebrew");
-    const $german = lib.$(".typewriter .german");
-    const $spanish = lib.$(".typewriter .spanish");
-    const $esperanto = lib.$(".typewriter .esperanto");
+    const hellos = [
+      { lang: "english", word: "Hello", dir: "ltr" },
+      { lang: "hebrew", word: "אהלן", dir: "rtl" },
+      { lang: "german", word: "Hallo", dir: "ltr" },
+      { lang: "spanish", word: "Hola", dir: "ltr" },
+      { lang: "esperanto", word: "Saluton", dir: "ltr" },
+    ];
 
-    const all = [$english, $hebrew, $german, $spanish, $esperanto];
     let current = 0;
 
-    const hideAll = () => {
-      all.forEach(($elem) => ($elem.style.display = "none"));
+    const createElement = (hello) => {
+      return `<h3 class="${hello.lang}" dir="${hello.dir}">${hello.word}</h3>`;
     };
 
     const showNext = () => {
-      if (current >= all.length) {
+      if (current >= hellos.length) {
         current = 0;
       }
 
-      hideAll();
-
-      all[current].style.display = "inline-block";
+      const $greeting = lib.$(".typewriter .greetings");
+      $greeting.innerHTML = createElement(hellos[current]);
       current += 1;
     };
 
     setTimeout(() => {
       showNext();
-      setInterval(showNext, 5000);
+      setInterval(showNext, 3500);
     }, 500);
   };
 
